c118cda9e96adccca96a1e0dd7e3fefce0bc60e2
Sven Jostkleigrewe <sven@jostkleigrewe.com>
2026-01-05 07:57:08 +0100

    WIP manage assets

--- src/Consent/CookieConfig.php
+++ src/Consent/CookieConfig.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace JostKleigrewe\CookieConsentBundle\Consent;
 
-final class CookieConfig
+class CookieConfig
 {
     public function __construct(
         public readonly string $name,
